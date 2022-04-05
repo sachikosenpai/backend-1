fce4da70e3fe5fe7eba929ad8f24885e64806883
sachikosenpai <popoolafeyisayo5@gmail.com>
2022-04-05 21:24:28 +0100

    first commit

---
+++ HelloworldApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace HelloworldApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string myName = ("Feyisayo Popoola");
+            Console.WriteLine(myName);
+            int num = (100);
+            Console.WriteLine(num);
+            float rate = (10.2f);
+            Console.WriteLine(rate);
+            decimal amount = (100.10M);
+            Console.WriteLine(amount);
+            long L = (45755452222222222L);
+            Console.WriteLine(L);
+            uint ui = (100u);
+            Console.WriteLine(ui);
+            ulong ul = (4575545222222222222ul);
+            Console.WriteLine(ul);
+            bool isValid = (true);
+            Console.WriteLine(isValid);
+            int num2 = (55);
+            Console.WriteLine(num2);
+            uint unit = (500u);
+            Console.WriteLine(unit);
+
+            int i = (75);
+            long j = (i);
+            Console.WriteLine(i);
+            double d = (75.25);
+            int k;
+            k = (int)d;
+            Console.WriteLine(k);
+
+        }
+    }
+}
---
+++ convertapp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace convertapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int x = 100;
+            int y = 22 + 15;
+            int z = 83 + 44;
+            int a = 77 + 3;
+            Console.WriteLine(x > y);
+            Console.WriteLine(x > z);
+            Console.WriteLine(x > a);
+        }
+    }
+}
